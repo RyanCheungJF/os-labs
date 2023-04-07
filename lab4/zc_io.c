c53d3f1e8c09de1a26b948898ddf9d2f771448ac
Ryan Cheung <ryan9cheung@gmail.com>
2023-04-08 00:12:31 +0800

    lab4: Add write functionality

--- lab4/zc_io.c
+++ lab4/zc_io.c
@@ -9,20 +9,18 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-// The zc_file struct is analogous to the FILE struct that you get from fopen.
+// the zc_file struct is analogous to the FILE struct that you get from fopen.
 struct zc_file
 {
-    /* Some suggested fields :
-        - pointer to the virtual memory space
-        - offset from the start of the virtual memory
-        - total size of the file
-        - file descriptor to the opened file
-        - mutex for access to the memory space and number of readers
-    */
+    // pointer to the virtual memory space
     void *fileLocation;
+    // offset from the start of the virtual memory
     size_t fileOffset;
+    // total size of the file
     size_t fileSize;
+    // file descriptor for the opened file
     int fileDescriptor;
+    // mutex for access to the memory space and number of readers
 };
 
 /**************
@@ -34,8 +32,8 @@ zc_file *zc_open(const char *path)
     // initialize file structure
     zc_file *file = (zc_file *)malloc(sizeof(zc_file));
 
-    // creates file from path if it does not exist, else open
-    int fd = open(path, O_CREAT | O_RDWR);
+    // creates file from path with read write exec permissions if it does not exist, else open
+    int fd = open(path, O_CREAT | O_RDWR, 00700);
     if (fd == -1)
     {
         perror("ERROR: Error when opening/ creating file!\n");
@@ -52,15 +50,19 @@ zc_file *zc_open(const char *path)
     file->fileSize = buffer.st_size;
     file->fileOffset = 0;
 
-    // address = NULL means kernel can map anywhere, length = no. of B to be mapped, protect = define access, we set to READ and WRITE
-    // flags = sharable by other processes, file descriptor, offset
-    // mmap helps to map virtual memory and allows us to access it like an array
-    void *fp = mmap(NULL, file->fileSize, PROT_READ | PROT_WRITE, MAP_SHARED, file->fileDescriptor, file->fileOffset);
-    if (fp == MAP_FAILED)
+    // we cannot map a file size of 0
+    if (file->fileSize != 0)
     {
-        perror("ERROR: Failed to map virtual memory!\n");
+        // address = NULL means kernel can map anywhere, length = no. of B to be mapped, protect = define access, we set to READ and WRITE
+        // flags = sharable by other processes, file descriptor, offset
+        // mmap helps to map virtual memory and allows us to access it like an array
+        void *fp = mmap(NULL, file->fileSize, PROT_READ | PROT_WRITE, MAP_SHARED_VALIDATE, file->fileDescriptor, file->fileOffset);
+        if (fp == MAP_FAILED)
+        {
+            perror("ERROR: Failed to map virtual memory for pre-existing file!\n");
+        }
+        file->fileLocation = fp;
     }
-    file->fileLocation = fp;
 
     // return pointer to struct
     return file;
@@ -104,11 +106,11 @@ const char *zc_read_start(zc_file *file, size_t *size)
     {
         *size = remainingSize;
     }
-    // read sys call reads up to the number of 'count' bytes given into the buffer
-    //  can cast a void pointer to a char pointer, commences at file offset
-    char *str = ((char *)file->fileLocation) + file->fileOffset;
+    // can cast a void pointer to a char pointer, commences at file offset
+    // since char* is just a string, this is basically just the starting addr of the first char + size of substring
+    char *dataPointer = ((char *)file->fileLocation) + file->fileOffset;
     file->fileOffset += *size;
-    return str;
+    return dataPointer;
 }
 
 void zc_read_end(zc_file *file)
@@ -118,13 +120,52 @@ void zc_read_end(zc_file *file)
 
 char *zc_write_start(zc_file *file, size_t size)
 {
-    // To implement
-    return NULL;
+    // cond 1: if file size is 0, it means we initially created a new file, and now want to write to it, so create virtual memory
+    // cond 2: alternatively, we have a pre existing file but of insufficient space
+    if (file->fileSize == 0 || file->fileOffset + size > file->fileSize)
+    {
+        void *fp;
+        if (file->fileSize == 0)
+        {
+            // this only maps the virtual memory, but does not change the file size of the created file (empty)
+            fp = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED_VALIDATE, file->fileDescriptor, file->fileOffset);
+            if (fp == MAP_FAILED)
+            {
+                perror("ERROR: Failed to map virtual memory for newly created file!\n");
+            }
+        }
+        else
+        {
+            // insufficient space, we need to remap, MREMAP_MAYMOVE signifies that kernel can relocate mapping if fails to expand
+            fp = mremap(file->fileLocation, file->fileSize, file->fileSize + size, MREMAP_MAYMOVE);
+            if (fp == MAP_FAILED)
+            {
+                perror("ERROR: Failed to remap virtual memory!\n");
+            }
+        }
+        file->fileLocation = fp;
+        file->fileSize += size;
+
+        // we then truncate to extend the size of the file, once we have sucessfully remapped
+        int status = ftruncate(file->fileDescriptor, file->fileOffset + size);
+        if (status == -1)
+        {
+            perror("Failed to truncate file size when writing!\n");
+        }
+    }
+    // sufficient space now, so just get the data
+    char *dataPointer = ((char *)file->fileLocation) + file->fileOffset;
+    file->fileOffset += size;
+    return dataPointer;
 }
 
 void zc_write_end(zc_file *file)
 {
-    // To implement
+    int syncStatus = msync(file->fileLocation, file->fileSize, MS_SYNC);
+    if (syncStatus == -1)
+    {
+        perror("ERROR: Failed to sync memory to file!\n");
+    }
 }
 
 /**************
